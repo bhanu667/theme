c8b7b3088665ef338996a9d755824bdd9b5349f5
bhanuprakash132 <72198277+bhanuprakash132@users.noreply.github.com>
2021-06-04 19:38:45 +0530

    Add project files.

---
+++ theme/Data/ContextSeed.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace theme.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Roles.SuperAdmin.ToString()));
+        }
+    }
+}
---
+++ theme/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace theme
+{
+        public enum Roles
+        {
+            Admin,
+            SuperAdmin,
+            User
+        }
+}
